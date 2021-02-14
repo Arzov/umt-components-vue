411e5c8d318057cf89e660d4b13ef77b63b3597c
Jesus Barrientos <jesus.barrientosg.95@gmail.com>
2021-02-14 19:54:05 -0300

    Completado componente UmtNavigationBar

---
+++ src/components/UmtNavigationBar.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="umt-component umt-navigation-bar">
+        <a-row type="flex" justify="space-around">
+            <a-col v-for="tab in tabs" :key="tab.key" class="tab">
+                <img v-if="isActive(tab.key)" :src="require(`./../assets/images/${mode}-${tab.icon}-active.svg`)" />
+                <img v-else :src="require(`./../assets/images/${mode}-${tab.icon}.svg`)" />
+                <span class="tab-name" :umt-active="isActive(tab.key)">{{ tab.name }}</span>
+            </a-col>
+        </a-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'UmtNavigationBar',
+        props: {
+            tabs: {
+                required: true,
+                type: Array
+            },
+
+            mode: {
+                required: false,
+                type: String,
+                default: 'lm'
+            },
+
+            activeTab: {
+                required: true,
+                type: String
+            }
+        },
+        methods: {
+            isActive(key) {
+                return this.activeTab === key
+            }
+        }
+    }
+</script>
