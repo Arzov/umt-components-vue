a6e0768c22b3183547c7970cda8130b09e52309f
Franco Barrientos <fjbarrientosg@gmail.com>
2021-06-23 23:59:36 -0400

    Diagonal buttons and new colors updated!

--- src/components/UmtButton.vue
+++ src/components/UmtButton.vue
@@ -1,71 +1,99 @@
 <template>
-    <div ref="button" class="umt-component umt-button" :umt-type="type" :umt-size="size" :umt-icon="icon != undefined" :umt-disabled="disabled" @click="onClick" @mouseenter="onMouseEnter" @mouseleave="onMouseLeave">
+
+    <div
+        ref="button"
+        class="umt-component umt-button"
+        :umt-type="type"
+        :umt-size="size"
+        :umt-disabled="disabled"
+        @click="onClick"
+        @mouseenter="onMouseEnter"
+        @mouseleave="onMouseLeave"
+    >
+
         <img v-if="icon" :src="_srcImage">
+
         <span>
             <slot></slot>
         </span>
+
     </div>
+
 </template>
 
+
 <script>
+
     import chroma from 'chroma-js'
     import { EasingFunctions } from './../assets/utils/easing'
     import { getImgPath } from '../statics/assets' 
 
+
     export default {
+
         name: 'UmtButton',
+
+
         props: {
+
             type: {
-                required: false,
-                type: String,
-                default: 'primary'
+                required    : false,
+                type        : String,
+                default     : 'primary'
             },
 
             size: {
-                required: false,
-                type: String,
-                default: 'normal'
+                required    : false,
+                type        : String,
+                default     : 'normal'
             },
 
             colors: {
-                required: false,
-                type: Object,
+                required    : false,
+                type        : Object,
                 default () {
                     return {}
                 }
             },
 
             icon: {
-                required: false,
-                type: String,
-                default: undefined
+                required    : false,
+                type        : String,
+                default     : undefined
             },
 
             disabled: {
-                required: false,
-                type: Boolean,
-                defsult: false
+                required    : false,
+                type        : Boolean,
+                defsult     : false
             }
+
         },
+
+
         data() {
             return {
-                currentTheme: 'light',
-                currentColorLeft: '',
-                currentColorRight: '',
-                currentColorBorder: '',
-                endColor: '',
-                initColorLeft: '',
-                initColorRight: '',
-                initColorBorder: '',
-                interval: undefined,
-                intervalDelay: 5,
-                intervalTimeout: 200
+                currentTheme        : 'light',
+                currentColorLeft    : '',
+                currentColorRight   : '',
+                currentColorBorder  : '',
+                endColor            : '',
+                initColorLeft       : '',
+                initColorRight      : '',
+                initColorBorder     : '',
+                interval            : undefined,
+                intervalDelay       : 5,
+                intervalTimeout     : 200
             }
         },
+
+
         mounted() {
+
             let themeElement = document.getElementById('theme-provider')
 
             if (themeElement != null) {
+
                 let style = getComputedStyle(themeElement)
                 this.currentTheme = themeElement.getAttribute('umt-theme')
                 this.endColor = style.getPropertyValue(this._colors.hoverColor)
@@ -75,45 +103,66 @@
                     this.currentColorRight = style.getPropertyValue(this._colors.rightColor)
                     this.initColorLeft = style.getPropertyValue(this._colors.leftColor)
                     this.initColorRight = style.getPropertyValue(this._colors.rightColor)
-                } else if (this.type == 'rounded') {
+                }
+
+                else if (this.type == 'rounded') {
                     this.currentColorBorder = style.getPropertyValue(this._colors.borderColor)
                     this.initColorBorder = style.getPropertyValue(this._colors.borderColor)
                 }
+
             }
+
         },
+
+
         computed: {
+
             _srcImage() {
                 return getImgPath(`${this.icon.toLowerCase()}.svg`)
             },
 
+
             _colors() {
+
                 let iconRightColor = '--button-bkg-color-right-alt'
 
                 return {
-                    leftColor: '--button-bkg-color-left',
-                    rightColor: this.icon ? iconRightColor : '--button-bkg-color-right',
-                    borderColor: '--button-border-color',
-                    hoverColor: '--button-bkg-color-hover',
+                    leftColor   : '--button-bkg-color-left',
+                    rightColor  : this.icon ? iconRightColor : '--button-bkg-color-right',
+                    borderColor : '--button-border-color',
+                    hoverColor  : '--button-bkg-color-hover',
                     ...this.colors
                 }
+
             }
+
         },
+
+
         watch: {
+
             currentColorLeft: function() {
-                this.$refs['button'].style.backgroundImage = this.getCssValuePrefix() + 'linear-gradient(45deg, ' + this.currentColorLeft + ', ' + this.currentColorRight + ')'
+                this.$refs['button'].style.backgroundImage = `${this.getCssValuePrefix()}linear-gradient(45deg, ${this.currentColorLeft}, ${this.currentColorRight})`
             },
 
+
             currentColorRight: function() {
-                this.$refs['button'].style.backgroundImage = this.getCssValuePrefix() + 'linear-gradient(45deg, ' + this.currentColorLeft + ', ' + this.currentColorRight + ')'
+                this.$refs['button'].style.backgroundImage = `${this.getCssValuePrefix()}linear-gradient(45deg, ${this.currentColorLeft}, ${this.currentColorRight})`
             },
+
 
             currentColorBorder: function() {
                 this.$refs['button'].style.color = this.currentColorBorder
                 this.$refs['button'].style.borderColor = this.currentColorBorder
             }
+
         },
+
+
         methods: {
+
             onClick($event) {
+
                 this.$emit('click', $event)
 
                 if (this.$umtMode == 'mobile') {
@@ -124,53 +173,73 @@
                         }, 100);
                     })
                 }
+
             },
+
 
             onMouseEnter() {
                 if (this.$umtMode == 'desktop')
                     this.addHover()
             },
 
+
             onMouseLeave() {
                 if (this.$umtMode == 'desktop')
                     this.removeHover()
             },
 
+
             addHover() {
+
                 if (this.type == 'primary') {
                     let chromaLeft = this.calculateColor(this.currentColorLeft, this.endColor).domain([0,1])
                     let chromaRight = this.calculateColor(this.currentColorRight, this.endColor).domain([0,1])
                     return this.playAnimation(chromaLeft, chromaRight)
-                } else if (this.type == 'rounded') {
+                }
+
+                else if (this.type == 'rounded') {
                     let chromaBorder = this.calculateColor(this.currentColorBorder, this.endColor).domain([0,1])
                     return this.playAnimation(chromaBorder, null)
                 }
+
             },
 
+
             removeHover() {
+
                 if (this.type == 'primary') {
                     let chromaLeft = this.calculateColor(this.currentColorLeft, this.initColorLeft).domain([0,1])
                     let chromaRight = this.calculateColor(this.currentColorRight, this.initColorRight).domain([0,1])
                     return this.playAnimation(chromaLeft, chromaRight)
-                } else if (this.type == 'rounded') {
+                }
+
+                else if (this.type == 'rounded') {
                     let chromaBorder = this.calculateColor(this.currentColorBorder, this.initColorBorder).domain([0,1])
                     return this.playAnimation(chromaBorder, null)
                 }
+
             },
 
+
             // Obtiene la gama de colores para la animación
+
             calculateColor(start, end) {
                 return chroma.scale([ start.trim(), end.trim() ]);
             },
 
+
             // Ejecuta la animación del botón al presionarlo
+
             playAnimation(chromaLeft, chromaRight) {
                 return new Promise((resolve) => {
+
                     if (this.interval)
                         clearInterval(this.interval)
 
                     let steps = this.intervalTimeout / this.intervalDelay
                     let currStep = 0
+
+
                     this.interval = setInterval(() => {
                         if (currStep <= steps) {
                             let t = (1/steps) * currStep
@@ -189,11 +258,13 @@
                             resolve()
                         }
                     }, this.intervalDelay)
+
                 })
             },
 
             getCssValuePrefix() {
-                var rtrnVal = ''//default to standard syntax
+
+                var rtrnVal = '' // default to standard syntax
                 var prefixes = ['-o-', '-ms-', '-moz-', '-webkit-']
 
                 // Create a temporary DOM object for testing
@@ -210,7 +281,11 @@
 
                 dom = null
                 return rtrnVal
+
             }
+
         }
+
     }
+
 </script>
